16158d13a01c26dd9fcd5a3139b6d37253058bca
Alex Ungureanu <ungureanualex11@yahoo.com>
2023-11-11 07:47:31 +0200

    added some images

--- zodiac-sphere-app/src/pages/Home.jsx
+++ zodiac-sphere-app/src/pages/Home.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import { ChakraProvider, Box, Heading } from "@chakra-ui/react";
+import React from "react";
+import { ChakraProvider, Box, Heading, Image, Flex } from "@chakra-ui/react";
 
-const  Home = () => {
+const Home = () => {
   return (
-    <Box >
-        <Heading> Homeee </Heading>
-        <Heading> Homeee </Heading>
-        <Heading> Homeee </Heading>
-        <Heading> Homeee </Heading>
-        <Heading> Homeee </Heading>
+    <Box>
+      <Heading> Homeee </Heading>
+      <Heading> Homeee </Heading>
+      <Heading> Homeee </Heading>
+      <Heading> Homeee </Heading>
+      <Heading> Homeee </Heading>
+
+      <Flex justifyContent="space-around">
+        <Image
+          src="/images/signs/scorpio.png"
+          alt="Logo"
+          h="200px"
+          _hover={{
+            transform: "scale(20) translateY(200px) translateX(300px) rotate(600deg)",
+            transition: "transform 10s",
+          }}
+        />
+        <Image
+          src="/images/signs/leo.png"
+          alt="Logo"
+          h="200px"
+          _hover={{
+            transform: "scale(10)",
+            transition: "transform 2s",
+          }}
+        />
+      </Flex>
     </Box>
   );
 };
