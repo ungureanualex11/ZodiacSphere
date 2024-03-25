41d2805500eb9f28c8e3d36ee781a98d7b8c7ac5
Alex Ungureanu <ungureanualex11@yahoo.com>
2024-03-25 03:39:50 +0200

    Navbar/Sidebar done

--- zodiac-sphere-app/src/Navbar.jsx
+++ zodiac-sphere-app/src/Navbar.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import {
   Box,
   ChakraProvider,
+  Container,
+  Center,
   Flex,
   Image,
   useBreakpointValue,
@@ -22,7 +24,7 @@ const Navbar = () => {
 
   useEffect(() => {
     if (isLargeScreen) {
-      setSidebarVisibility(false); 
+      setSidebarVisibility(false);
     }
   }, [isLargeScreen]);
 
@@ -42,155 +44,152 @@ const Navbar = () => {
 
   return (
     <ChakraProvider>
+      {!sidebarVisibility && (
+        <Flex
+          direction={{ base: "column", lg: "row" }}
+          justifyContent="space-around"
+          bg="#141d39"
+          p="6px"
+          top="0px"
+          position="sticky"
+          width="100%"
+          borderBottom="2px solid #a3abc9"
+          zIndex="1000"
+        >
+          {/* Top menu content */}
+          <Flex direction="row" justifyContent="space-between" align="center">
+            <Link to="/" onClick={closeSidebar}>
+              <Image
+                src="/images/logo.png"
+                alt="Logo"
+                h={{ base: "50px", sm: "60px", md: "70px", lg: "80px" }}
+                style={{ filter: "invert(1)" }}
+                _hover={{
+                  transform: "scale(1.14) rotate(360deg)",
+                  transition: "transform 0.6s",
+                }}
+              />
+            </Link>
 
-     {!sidebarVisibility && ( 
-      <Flex
-        direction={{ base: "column", lg: "row" }}
-        justifyContent="space-around"
-        bg="#141d39"
-        p="6px"
-        top="0px"
-        position="sticky"
-        width="100%"
-        borderBottom="2px solid #a3abc9"
-        zIndex="1000"
-      >
-        {/* Top menu content */}
-        <Flex direction="row" justifyContent="space-between" align="center">
-          <Link to="/" onClick={closeSidebar}>
             <Image
-              src="/images/logo.png"
-              alt="Logo"
-              h={{ base: "50px", sm: "60px", md: "70px", lg: "80px" }}
-              style={{ filter: "invert(1)" }}
+              src="/images/hamburger_icon_open.png"
+              alt="Menu icon"
+              h={{ base: "30px", sm: "50px" }}
+              onClick={toggleSidebar}
+              display={{ base: "block", lg: "none" }}
+              m="5px"
+              mt="10px"
               _hover={{
-                transform: "scale(1.14) rotate(360deg)",
-                transition: "transform 0.6s",
+                transform: "scale(1.2)",
+                transition: "transform 0.4s",
+                cursor: "pointer",
               }}
             />
-          </Link>
+          </Flex>
 
-          <Image
-            src={
-              sidebarVisibility
-                ? "/images/hamburger_icon_close.png"
-                : "/images/hamburger_icon_open.png"
-            }
-            alt="Menu icon"
-            h={{ base: "30px", sm: "50px" }}
-            onClick={toggleSidebar}
-            display={{ base: "block", lg: "none" }}
-            m="5px"
-            mt="10px"
-            _hover={{
-              transform: "scale(1.2)",
-              transition: "transform 0.4s",
-              cursor: "pointer",
-            }}
-          />
+          <Flex align="center" justifyContent="space-between">
+            {isLargeScreen && (
+              <>
+                <Link to="/" onClick={closeSidebar}>
+                  <Box
+                    m="5px"
+                    fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
+                    borderWidth="2px"
+                    borderColor="#a3abc9"
+                    borderRadius="2xl"
+                    color="#e6ebff"
+                    p="12px"
+                    pl="25px"
+                    pr="25px"
+                    _hover={{
+                      bgColor: "#141d50",
+                      transform: "scale(1.1)",
+                      transition: "transform 0.2s",
+                    }}
+                  >
+                    Home
+                  </Box>
+                </Link>
+
+                <Link to="/find-your-sign" onClick={closeSidebar}>
+                  <Box
+                    m="5px"
+                    fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
+                    borderWidth="2px"
+                    borderColor="#a3abc9"
+                    borderRadius="2xl"
+                    color="#e6ebff"
+                    p="12px"
+                    pl="25px"
+                    pr="25px"
+                    _hover={{
+                      bgColor: "#141d50",
+                      transform: "scale(1.1)",
+                      transition: "transform 0.2s",
+                    }}
+                  >
+                    Find your sign
+                  </Box>
+                </Link>
+
+                <Link to="/about-your-sign" onClick={closeSidebar}>
+                  <Box
+                    m="5px"
+                    fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
+                    borderWidth="2px"
+                    borderColor="#a3abc9"
+                    borderRadius="2xl"
+                    color="#e6ebff"
+                    p="12px"
+                    pl="25px"
+                    pr="25px"
+                    _hover={{
+                      bgColor: "#141d50",
+                      transform: "scale(1.1)",
+                      transition: "transform 0.2s",
+                    }}
+                  >
+                    About your sign
+                  </Box>
+                </Link>
+
+                <Link to="/compatibility" onClick={closeSidebar}>
+                  <Box
+                    m="5px"
+                    fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
+                    borderWidth="2px"
+                    borderColor="#a3abc9"
+                    borderRadius="2xl"
+                    color="#e6ebff"
+                    p="12px"
+                    pl="25px"
+                    pr="25px"
+                    _hover={{
+                      bgColor: "#141d50",
+                      transform: "scale(1.1)",
+                      transition: "transform 0.2s",
+                    }}
+                  >
+                    Compatibility
+                  </Box>
+                </Link>
+              </>
+            )}
+          </Flex>
         </Flex>
-
-        <Flex align="center" justifyContent="space-between">
-          {isLargeScreen && (
-            <>
-              <Link to="/" onClick={closeSidebar}>
-                <Box
-                  m="5px"
-                  fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
-                  borderWidth="2px"
-                  borderColor="#a3abc9"
-                  borderRadius="2xl"
-                  color="#e6ebff"
-                  p="12px"
-                  pl="25px"
-                  pr="25px"
-                  _hover={{
-                    bgColor: "#141d50",
-                    transform: "scale(1.1)",
-                    transition: "transform 0.2s",
-                  }}
-                >
-                  Home
-                </Box>
-              </Link>
-
-              <Link to="/find-your-sign" onClick={closeSidebar}>
-                <Box
-                  m="5px"
-                  fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
-                  borderWidth="2px"
-                  borderColor="#a3abc9"
-                  borderRadius="2xl"
-                  color="#e6ebff"
-                  p="12px"
-                  pl="25px"
-                  pr="25px"
-                  _hover={{
-                    bgColor: "#141d50",
-                    transform: "scale(1.1)",
-                    transition: "transform 0.2s",
-                  }}
-                >
-                  Find your sign
-                </Box>
-              </Link>
-
-              <Link to="/about-your-sign" onClick={closeSidebar}>
-                <Box
-                  m="5px"
-                  fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
-                  borderWidth="2px"
-                  borderColor="#a3abc9"
-                  borderRadius="2xl"
-                  color="#e6ebff"
-                  p="12px"
-                  pl="25px"
-                  pr="25px"
-                  _hover={{
-                    bgColor: "#141d50",
-                    transform: "scale(1.1)",
-                    transition: "transform 0.2s",
-                  }}
-                >
-                  About your sign
-                </Box>
-              </Link>
-
-              <Link to="/compatibility" onClick={closeSidebar}>
-                <Box
-                  m="5px"
-                  fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
-                  borderWidth="2px"
-                  borderColor="#a3abc9"
-                  borderRadius="2xl"
-                  color="#e6ebff"
-                  p="12px"
-                  pl="25px"
-                  pr="25px"
-                  _hover={{
-                    bgColor: "#141d50",
-                    transform: "scale(1.1)",
-                    transition: "transform 0.2s",
-                  }}
-                >
-                  Compatibility
-                </Box>
-              </Link>
-            </>
-          )}
-        </Flex>
-      </Flex>
       )}
- {sidebarVisibility && (
+      {sidebarVisibility && (
         <Flex
-          paddingTop="5%"
+          paddingTop="17%"
           paddingRight="5%"
           paddingLeft="5%"
+          paddingBottom="20%"
           borderRight="2px solid #a3abc9"
           ref={sidebarRef}
           direction="column"
+          justifyContent="space-between"
           bg="#141d39"
-          width="75%" 
+          width="75%"
           height="100vh"
           position="fixed"
           top="0"
@@ -198,105 +197,110 @@ const Navbar = () => {
           zIndex="999"
         >
           {/* Sidebar content */}
+          <Box>
           <Link to="/" onClick={closeSidebar}>
-  <Box
-    display="flex"
-    alignItems="center"
-    justifyContent="center"
-    m="5px"
-    fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
-    borderWidth="2px"
-    borderColor="#a3abc9"
-    borderRadius="2xl"
-    color="#e6ebff"
-    p="8px"
-    _hover={{
-      bgColor: "#141d50",
-      transform: "scale(1.1)",
-      transition: "transform 0.2s",
-    }}
-  >
-    Home
-  </Box>
-</Link>
-
-<Link to="/find-your-sign" onClick={closeSidebar}>
-  <Box
-    display="flex"
-    alignItems="center"
-    justifyContent="center"
-    m="5px"
-    fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
-    borderWidth="2px"
-    borderColor="#a3abc9"
-    borderRadius="2xl"
-    color="#e6ebff"
-    p="8px"
-    _hover={{
-      bgColor: "#141d50",
-      transform: "scale(1.1)",
-      transition: "transform 0.2s",
-    }}
-  >
-    Find your sign
-  </Box>
-</Link>
-
-<Link to="/about-your-sign" onClick={closeSidebar}>
-  <Box
-    display="flex"
-    alignItems="center"
-    justifyContent="center"
-    m="5px"
-    fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
-    borderWidth="2px"
-    borderColor="#a3abc9"
-    borderRadius="2xl"
-    color="#e6ebff"
-    p="8px"
-    _hover={{
-      bgColor: "#141d50",
-      transform: "scale(1.1)",
-      transition: "transform 0.2s",
-    }}
-  >
-    About your sign
-  </Box>
-</Link>
-
-<Link to="/compatibility" onClick={closeSidebar}>
-  <Box
-    display="flex"
-    alignItems="center"
-    justifyContent="center"
-    m="5px"
-    fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
-    borderWidth="2px"
-    borderColor="#a3abc9"
-    borderRadius="2xl"
-    color="#e6ebff"
-    p="8px"
-    _hover={{
-      bgColor: "#141d50",
-      transform: "scale(1.1)",
-      transition: "transform 0.2s",
-    }}
-  >
-    Compatibility
-  </Box>
-</Link>
-<Image
-              marginTop="60%"
-              src="/images/logo.png"
-              alt="Logo"
-              h="15%"
-              style={{ filter: "invert(1)" }}
-              objectFit="contain"
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              marginTop="3%"
+              m="5px"
+              fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
+              borderWidth="2px"
+              borderColor="#a3abc9"
+              borderRadius="2xl"
+              color="#e6ebff"
+              p="8px"
               _hover={{
-                transform: "scale(1.14) rotate(360deg)",
-                transition: "transform 0.6s",
+                bgColor: "#141d50",
+                transform: "scale(1.1)",
+                transition: "transform 0.2s",
               }}
-            />
+            >
+              Home
+            </Box>
+          </Link>
+
+          <Link to="/find-your-sign" onClick={closeSidebar}>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              m="5px"
+              marginTop="3%"
+              fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
+              borderWidth="2px"
+              borderColor="#a3abc9"
+              borderRadius="2xl"
+              color="#e6ebff"
+              p="8px"
+              _hover={{
+                bgColor: "#141d50",
+                transform: "scale(1.1)",
+                transition: "transform 0.2s",
+              }}
+            >
+              Find your sign
+            </Box>
+          </Link>
+
+          <Link to="/about-your-sign" onClick={closeSidebar}>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              m="5px"
+              marginTop="3%"
+              fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
+              borderWidth="2px"
+              borderColor="#a3abc9"
+              borderRadius="2xl"
+              color="#e6ebff"
+              p="8px"
+              _hover={{
+                bgColor: "#141d50",
+                transform: "scale(1.1)",
+                transition: "transform 0.2s",
+              }}
+            >
+              About your sign
+            </Box>
+          </Link>
+
+          <Link to="/compatibility" onClick={closeSidebar}>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              m="5px"
+              marginTop="3%"
+              fontSize={{ base: "xs", sm: "md", md: "lg", lg: "xl" }}
+              borderWidth="2px"
+              borderColor="#a3abc9"
+              borderRadius="2xl"
+              color="#e6ebff"
+              p="8px"
+              _hover={{
+                bgColor: "#141d50",
+                transform: "scale(1.1)",
+                transition: "transform 0.2s",
+              }}
+            >
+              Compatibility
+            </Box>
+          </Link>
+          </Box>
+          <Image
+            src="/images/logo.png"
+            alt="Logo"
+            h="15%"
+            style={{ filter: "invert(1)" }}
+            objectFit="contain"
+            _hover={{
+              transform: "scale(1.14) rotate(360deg)",
+              transition: "transform 0.6s",
+            }}
+          />
         </Flex>
       )}
     </ChakraProvider>
